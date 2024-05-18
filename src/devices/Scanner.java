1fd56aff337ba258501a45e92a40dac401fb996e
Thiago Antonio <thiagoapsx@gmail.com>
2024-05-18 18:25:41 -0300

    interface class: creating the class combo device

--- src/devices/Scanner.java
+++ src/devices/Scanner.java
@@ -1,17 +1,6 @@
 package devices;
 
-public class Scanner extends Device{
-
-	public Scanner(String serialNumber) {
-		super(serialNumber);
-	}
+public interface Scanner {
 	
-	@Override
-	public void processDoc(String doc) {
-		System.out.println("Scanner processing: " + doc);
-	}
-
-	public String scan() {
-		return "Scanned content";
-	}
+	String scan();
 }
